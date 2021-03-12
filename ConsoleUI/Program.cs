566635df3283635f5aab6ef80bf4ed5e4c8614d9
Elif <elifintizam@gmail.com>
2021-03-12 20:57:50 +0300

    Core katmanına Result yapısı eklendi.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -27,11 +27,23 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
+            var result = productManager.GetProductDetails();
+            if (result.Success==true)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
 
-            foreach (var product in productManager.GetProductDetails())
+            /*foreach (var product in productManager.GetProductDetails())
             {
                 Console.WriteLine(product.ProductName + "/" + product.CategoryName );
-            }
+            }*/
         }
     }
 }
---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public interface IDataResult<T> : IResult //sen aynı zamanda bir IResultsın ve ek olarak T türünde data içereceksin
+    {
+        T Data { get; }
+
+    }
+}
